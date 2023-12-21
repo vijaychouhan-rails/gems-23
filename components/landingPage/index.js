2473df8fabed7f1d0a2796391d6aca2f1c14fe9e
Vijay <vijaychouhan.rails@gmail.com>
2023-12-21 13:41:41 +0530

    Top section WIP

--- components/landingPage/index.js
+++ components/landingPage/index.js
@@ -2,11 +2,14 @@ import Head from "next/head";
 import React from "react";
 import HeadPage from "../layout/headPage";
 import HEAD_TITLES from "@/utils/constants/titleConstants";
+import TopSection from "./topSection";
 
 function LandingPage() {
   return (
     <HeadPage title={HEAD_TITLES.home}>
-      <div>This is the landing page of the website</div>
+      <main>
+        <TopSection />
+      </main>
     </HeadPage>
   );
 }
--- components/layout/index.js
+++ components/layout/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import HeaderPage from './header'
-import FooterPage from './footerPage'
+import React from "react";
+import HeaderPage from "./header";
+import FooterPage from "./footerPage";
 
 function LayoutPage({ children }) {
   return (
     <>
-        <HeaderPage/>
-        {children}
-        <FooterPage />
+      <HeaderPage />
+      {children}
+      {/* <FooterPage /> */}
     </>
-  )
+  );
 }
 
-export default LayoutPage
+export default LayoutPage;
