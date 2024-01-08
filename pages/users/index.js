21916e4c7c47bf04fdfe46842ef8caf7caaab3b4
Vijay <vijaychouhan.rails@gmail.com>
2024-01-08 13:12:48 +0530

    Middleware

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,29 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import LayoutPage from "@/components/layout";
 import UsersPage from "@/components/usersPage";
-import Cookies from "js-cookie";
-import { useRouter } from "next/router";
-import useAuth from "@/utils/hooks/useAuth";
-import withSafeRoute from "@/components/layout/withSafeRoute";
 
 function Users() {
-  const router = useRouter();
-  // const { validated } = useAuth();
-  const validated = true;
-
-  // useEffect(() => {
-  //   if (!validated) {
-  //     router.replace("/login");
-  //   }
-  // }, [validated]);
-
-  if (validated) {
-    return <UsersPage />;
-  }
+  return <UsersPage />;
 }
 
 Users.getLayout = function getLayout(page) {
   return <LayoutPage>{page}</LayoutPage>;
 };
 
-export default withSafeRoute(Users);
+// export default withSafeRoute(Users);
+export default Users;
